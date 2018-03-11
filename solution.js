c0ab260671eb84b4067e4ac93766f09669ef04eb
Jesse Atay <jesse.atay@gmail.com>
2018-03-11 14:55:50 -0700

    updated initial files

---
+++ solution.js
@@ -0,0 +1,10 @@
+function checkExam(array1, array2) {
+    var scoreCount = 0;
+     for(var i = 0; i < array1.length; i++){
+     if(array2[i] === array1[i]){
+     scoreCount += 4;
+     } else if {
+     
+     }
+     
+    }
