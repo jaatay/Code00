633c2c6cadc505484808fb1872dee8ad4661be92
Jesse Atay <jesse.atay@gmail.com>
2018-03-12 14:42:00 -0700

    copied and pasted from Code Wars after passing tests

--- solution.js
+++ solution.js
@@ -1,10 +1,16 @@
 function checkExam(array1, array2) {
     var scoreCount = 0;
      for(var i = 0; i < array1.length; i++){
-     if(array2[i] === array1[i]){
-     scoreCount += 4;
-     } else if {
-     
-     }
-     
-    }
+        if(array2[i] === ''){
+        scoreCount += 0;
+        } else if(array2[i] !== array1[i]){
+        scoreCount -= 1;
+        } else if(array2[i] === array1[i]){
+        scoreCount += 4;
+        }
+    }   
+  if(scoreCount < 0){
+  scoreCount = 0;
+  }
+  return scoreCount;
+};
